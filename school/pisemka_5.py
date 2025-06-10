8243c86aa7a1cf823f2e8aa5149c29b6a115d71c
Mo6uL <dayan.batbold024@skolavdf.cz>
2025-06-10 09:10:50 +0200

    pisemka

---
+++ school/pisemka_5.py
@@ -0,0 +1,25 @@
+x = 2 
+y = 1
+print = x//y
+
+L = [1, 2, 3]
+print(sum(l)
+
+x = 1
+y = 2
+z = 3
+print((x+y+z)/3)
+
+min = 1
+max = 10
+for i in range(min, max + 1):
+  print(i)
+
+min = 1
+max = 10
+cislo = 5
+print(min < cislo < max)
+
+
+
+      
